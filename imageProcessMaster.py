07cbb4b452edf125276d4cf634396f53f0370cb0
zhenwork <zhensu@stanford.edu>
2018-04-20 16:52:00 -0700

    zhen

--- imageProcessMaster.py
+++ imageProcessMaster.py
@@ -5,19 +5,18 @@ import argparse
 comm_rank = MPI.COMM_WORLD.Get_rank()
 comm_size = MPI.COMM_WORLD.Get_size()
 parser = argparse.ArgumentParser()
-parser.add_argument("-i","--i", help="raw file folder", default="/", type=str)
-parser.add_argument("-o","--o", help="save folder", default="/", type=str)
+parser.add_argument("-o","--o", help="save folder", default=".", type=str)
 parser.add_argument("-num","--num", help="num of images to process", default=-1, type=int)
 args = parser.parse_args()
 
 zf = iFile()
 if args.num==-1:	
-	num = zf.counterFile(args.i, title='.slice')
+	num = zf.counterFile(args.o+'/rawImage', title='.slice')
 else: num = int(args.num)
 sep = np.linspace(0, num, comm_size+1).astype('int')
 
 
-fname = args.i+'/rawImage_00000.slice'
+fname = args.o+'/rawImage/rawImage_00000.slice'
 Geo = zf.get_image_info(fname)
 assert Geo['readout']=='image'
 image = zf.h5reader(fname, 'image')
@@ -26,21 +25,29 @@ ascale = solid_angle_correction(image, Geo)
 pscale = polarization_correction(image, Geo)
 apscale = ascale * pscale
 apscale /= np.amax(apscale)
+apscale = 1./apscale
+ascale  = 1./ascale
+pscale  = 1./pscale
+
 if comm_rank == 0:
 	print '## Total number: '+str(num).rjust(5)
-	zf.h5writer(args.i+'/ascale.scale', 'scale', ascale)
-	zf.h5writer(args.i+'/pscale.scale', 'scale', pscale)
-	zf.h5writer(args.i+'/apscale.scale', 'scale', apscale)
+	if not os.path.exists(args.o + '/mergeImage'): 
+		os.mkdir(args.o + '/mergeImage')
+	zf.h5modify(args.o+'/image.process', 'ascale' , ascale)
+	zf.h5modify(args.o+'/image.process', 'pscale' , pscale)
+	zf.h5modify(args.o+'/image.process', 'apscale', apscale)
+else:
+	while not os.path.exists(args.o + '/mergeImage'): pass
 
-mask = zf.h5reader(args.i+'/mask.mask')
+mask = zf.h5reader(args.o+'/image.process', 'mask')
 for idx in range(sep[comm_rank], sep[comm_rank+1]):
-	fname = args.i+'/rawImage_'+str(idx).zfill(5)+'.slice'
+	fname = args.o+'/rawImage/rawImage_'+str(idx).zfill(5)+'.slice'
 	Geo = zf.get_image_info(fname)
 	assert Geo['readout']=='image'
 	image = zf.h5reader(fname, 'image')
 
-	image /= apscale
-	image *= Geo['scale']
+	image *= apscale
+	#image *= Geo['scale']
 	image = remove_peak_alg1(image, mask=mask, sigma=15, cwin=(11,11))
 
 	fsave = args.o + '/mergeImage_'+str(idx).zfill(5)+'.slice'
--- scalingFactor.py
+++ scalingFactor.py
@@ -1,10 +1,16 @@
-import numpy as np 
 from mpidata import *
 from userScript import *
 from mpi4py import MPI
 comm_rank = MPI.COMM_WORLD.Get_rank()
 comm_size = MPI.COMM_WORLD.Get_size()
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("-i","--i", help="raw file folder", default=".", type=str)
+parser.add_argument("-num","--num", help="num of images to process", default=-1, type=int)
+args = parser.parse_args()
+num = args.num
 
+sep = np.linspace(0, N, comm_size+1).astype('int')
 for i in range(N):
 	Image_i = 
 	for j in range(N):
