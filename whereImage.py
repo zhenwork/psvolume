c3f5aaded37fcf35b3f7d0a52f859be3c9ebf4bc
zhenwork <zhensu@stanford.edu>
2018-04-20 15:23:28 -0700

    zhen
    modify

--- whereImage.py
+++ whereImage.py
@@ -14,7 +14,6 @@ parser.add_argument("-num","--num", help="num of images to process", default=-1,
 args = parser.parse_args()
 ## folder = '/reg/data/ana04/users/zhensu/xpptut/volume/rawImage'
 
-
 # computation
 lsima = np.sqrt(np.sum(sima**2))
 lsimb = np.sqrt(np.sum(simb**2))
@@ -30,8 +29,10 @@ rot_three = np.linalg.inv(np.array([sima,simb,simc]).T)*Geo['pixelSize']/Geo['wa
 mask = makeMask()
 Mask = expand_mask(mask, cwin=(2,2), value=0)
 
-Filename = args.o+'/image.process'
+
 if comm_rank == 0:
+	if not os.path.exists(args.o + '/rawImage'): os.mkdir(args.o + '/rawImage')
+	Filename = args.o+'/image.process'
 	zf.h5writer(Filename, 'mask', mask)
 	zf.h5modify(Filename, 'Mask', Mask)
 
@@ -42,7 +43,7 @@ for idx in range(sep[comm_rank], sep[comm_rank+1]):
 	rot_one = Quat2Rotation(quaternion)
 	matrix = rot_three.dot(rot_three.dot(rot_one))
 
-	fsave = folder + '/rawImage_'+str(idx).zfill(5)+'.slice'
+	fsave = args.o + '/rawImage' + '/rawImage_'+str(idx).zfill(5)+'.slice'
 	zf.h5writer(fsave, 'readout', 'image') 
 	zf.h5modify(fsave, 'image', image*mask)
 	zf.h5modify(fsave, 'center', user_get_center(idx))
