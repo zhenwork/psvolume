efe4b886af42ddf194aaa36b76429f6527dd6d6a
zhenwork <zhensu@stanford.edu>
2018-04-24 12:49:52 -0700

    update

--- TEST_MPI.py
+++ TEST_MPI.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np 
 from mpidata import *
 from fileManager import iFile
@@ -20,40 +21,3 @@ Vol['volumeSampling'] = 1
 Vol['volumeSize'] = 2*Vol['volumeCenter']+1
 model3d = np.zeros([Vol['volumeSize']]*3)
 weight  = np.zeros([Vol['volumeSize']]*3)
-
-if comm_rank == 0:
-	fsave = zf.makeFolder(args.o, title='sp')
-	print "Folder: ", fsave
-	for nrank in range(comm_size-1):
-		md=mpidata()
-		md.recv()
-		model3d += md.model3d
-		weight += md.weight
-		recvRank = md.small.rank
-		md = None
-		print '### received file from ' + str(recvRank).rjust(2)
-
-	model3d = ModelScaling(model3d, weight)
-	pathIntens = fsave+'/merge.volume'
-	ThisFile = zf.readtxt(os.path.realpath(__file__))
-	zf.h5writer(pathIntens, 'execute', ThisFile)
-	zf.h5modify(pathIntens, 'intens', model3d, chunks=(1, Vol['volumeSize'], Vol['volumeSize']), opts=7)
-	zf.h5modify(pathIntens, 'weight', weight,  chunks=(1, Vol['volumeSize'], Vol['volumeSize']), opts=7)
-
-else:
-	sep = np.linspace(0, num, comm_size).astype('int')
-	for idx in range(sep[comm_rank-1], sep[comm_rank]):
-		fname = args.o+'/mergeImage/mergeImage_'+str(idx).zfill(5)+'.slice'
-		image = zf.h5reader(fname, 'image')
-		Geo = zf.get_image_info(fname)
-		image /= Geo['scale']
-		print '### rank ' + str(comm_rank).rjust(2) + ' is processing file: '+str(idx)+'/'+str(num)
-		[model3d, weight] = ImageMerge(model3d, weight, image, Geo, Vol)
-
-	print '### rank ' + str(comm_rank).rjust(2) + ' is sending file ... '
-	md=mpidata()
-	md.addarray('model3d', model3d)
-	md.addarray('weight', weight)
-	md.small.rank = comm_rank
-	md.send()
-	md = None
