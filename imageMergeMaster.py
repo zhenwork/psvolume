c7b4e173d73a522ecf4b6e2f9aaad124bfa7b047
zhenwork <zhensu@stanford.edu>
2018-04-23 18:45:17 -0700

    update

--- imageMergeMaster.py
+++ imageMergeMaster.py
@@ -2,6 +2,7 @@ from mpidata import *
 from fileManager import *
 from imageMergeClient import *
 import argparse
+from mpi4py import MPI
 comm_rank = MPI.COMM_WORLD.Get_rank()
 comm_size = MPI.COMM_WORLD.Get_size()
 parser = argparse.ArgumentParser()
