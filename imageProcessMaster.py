ba843400b3822191a3b3af00aa13e3cd426cf616
zhenwork <zhensu@stanford.edu>
2018-04-25 15:26:20 -0700

    update

--- imageProcessMaster.py
+++ imageProcessMaster.py
@@ -73,5 +73,6 @@ for idx in range(sep[comm_rank], sep[comm_rank+1]):
 	zf.h5modify(fsave, 'rot', 'matrix')
 	zf.h5modify(fsave, 'rotation', Geo['rotation'])
 	zf.h5modify(fsave, 'scale', Geo['scale'])
+	zf.h5modify(fsave, 'Umatrix', Geo['Umatrix'])
 	print '## Finished image:'+str(idx).rjust(5)+'/'+str(num) + '  sumIntens: '+str(sumIntens).ljust(10)
 	if idx>5000: break
