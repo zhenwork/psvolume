7a3f053d959e720abc8b7d31ff8a21081c80b244
zhenwork <zhensu@stanford.edu>
2018-04-25 05:15:55 -0700

    update

--- imageMergeMaster.py
+++ imageMergeMaster.py
@@ -6,6 +6,7 @@ from imageMergeClient import *
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("-o","--o", help="save folder", default=".", type=str)
+parser.add_argument("-U","--U", help="matrix", default="hkl", type=str)
 parser.add_argument("-num","--num", help="num of images to process", default=-1, type=int)
 parser.add_argument("-volumeSampling","--volumeSampling", help="num of images to process", default=1, type=int)
 parser.add_argument("-volumeCenter","--volumeCenter", help="num of images to process", default=60, type=int)
@@ -14,7 +15,7 @@ args = parser.parse_args()
 zf = iFile()
 if args.num==-1: num = zf.counterFile(args.o+'/mergeImage', title='.slice')
 else: num = int(args.num)
-#num = int(args.num)
+
 
 Vol = {}
 Vol['volumeCenter'] = int(args.volumeCenter)
@@ -27,6 +28,7 @@ if comm_rank == 0:
 	fsave = zf.makeFolder(args.o, title='sr')
 	print "### Folder: ", fsave
 	print "### Images: ", num
+	print "### Mode: ", args.U
 	print "### Volume: ", model3d.shape
 	print "### Center: ", Vol['volumeCenter']
 	print "### Sampling: ", Vol['volumeSampling']
@@ -36,17 +38,21 @@ if comm_rank == 0:
 		md.recv()
 		model3d += md.model3d
 		weight += md.weight
+		Umatrix = md.Umatrix
 		recvRank = md.small.rank
 		md = None
 		print '### received file from ' + str(recvRank).rjust(2) + '/' + str(comm_size)
 
 	model3d = ModelScaling(model3d, weight)
 	pathIntens = fsave+'/merge.volume'
+	if args.U == 'xyz': Umatrix = np.eye(3)
+
 	print "### saving File: ", pathIntens
 	ThisFile = zf.readtxt(os.path.realpath(__file__))
 	zf.h5writer(pathIntens, 'execute', ThisFile)
 	zf.h5modify(pathIntens, 'intens', model3d, chunks=(1, Vol['volumeSize'], Vol['volumeSize']), opts=7)
 	zf.h5modify(pathIntens, 'weight', weight,  chunks=(1, Vol['volumeSize'], Vol['volumeSize']), opts=7)
+	zf.h5modify(pathIntens, 'Umatrix', Umatrix)
 
 else:
 	sep = np.linspace(0, num, comm_size).astype('int');
@@ -56,14 +62,18 @@ else:
 		image = zf.h5reader(fname, 'image')
 		Geo = zf.get_image_info(fname)
 		Geo['Umatrix'] = zf.h5reader(fname, 'Umatrix')
-		#image /= Geo['scale']
+		
 		print '### rank ' + str(comm_rank).rjust(2) + ' is processing file: '+str(sep[comm_rank-1])+'/'+str(idx)+'/'+str(sep[comm_rank])
-		[model3d, weight] = ImageMerge_XYZ(model3d, weight, image, Geo, Vol)
+		if args.U=='xyz': 
+			[model3d, weight] = ImageMerge_XYZ(model3d, weight, image, Geo, Vol)
+		else: 
+			[model3d, weight] = ImageMerge_HKL(model3d, weight, image, Geo, Vol)
 
 	print '### rank ' + str(comm_rank).rjust(2) + ' is sending file ... '
 	md=mpidata()
 	md.addarray('model3d', model3d)
 	md.addarray('weight', weight)
+	md.addarray('Umatrix', Geo['Umatrix'])
 	md.small.rank = comm_rank
 	md.send()
 	md = None
