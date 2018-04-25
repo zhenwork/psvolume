5bac37633029434ba745bd96ebbe108366ecfd48
zhenwork <zhensu@stanford.edu>
2018-04-25 05:33:51 -0700

    update

--- volumeProcess.py
+++ volumeProcess.py
@@ -7,15 +7,15 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-i","--i", help="save folder", default=".", type=str)
 parser.add_argument("-thrmin","--thrmin", help="min value", default=-100, type=int)
 parser.add_argument("-thrmax","--thrmax", help="max value", default=1000, type=int)
-parser.add_argument("-name","--name", help="name", default="volume", type=str)
+parser.add_argument("-name","--name", help="name", default=".", type=str)
 args = parser.parse_args()
 thr = (args.thrmin, args.thrmax)
 
 # read volume geometry
 Umatrix = zf.h5reader(args.i, 'Umatrix')
-astar = Umatrix[0,:].copy()
-bstar = Umatrix[1,:].copy()
-cstar = Umatrix[2,:].copy()
+astar = Umatrix[:,0].copy()
+bstar = Umatrix[:,1].copy()
+cstar = Umatrix[:,2].copy()
 print '### astar = ', astar
 print '### bstar = ', bstar
 print '### cstar = ', cstar
@@ -29,37 +29,24 @@ print '### symmetrizing data ...'
 symData = lauesym(rawData, ithreshold=thr)
 print '### max/min: ', np.amin(symData), np.amax(symData)
 
-print '### distribuction calculation ... '
-[backg, radius] = distri(symhkl, astar, bstar, cstar, ithreshold=thr, iscale=1, iwindow=5)
+print '### background calculation ... '
+[backg, radius] = distri(symData, astar, bstar, cstar, ithreshold=thr, iscale=2, iwindow=4)
 print '### bgd max/min: ', np.amin(backg), np.amax(backg)
 
 #backg = meanf(backg, _scale=5, clim=(0.1, 50))
 
-symhkl_sub = symhkl.copy()
-[nnxx, nnyy, nnzz] = symhkl.shape
+print '### subtracting background ... '
+subData = symData.copy()
+[nnxx, nnyy, nnzz] = symData.shape
 for i in range(nnxx):
 	for j in range(nnyy):
 		for k in range(nnzz):
 			intr = radius[i,j,k]
-			symhkl_sub[i,j,k] -= backg[intr]
+			subData[i,j,k] -= backg[intr]
+print '### max/min: ', np.amin(subData), np.amax(subData)
 
-print 'aniso max/min: ', np.amin(symhkl_sub), np.amax(symhkl_sub)
 
-
-print '### volumelist calculation ... '
-volumelist = hkl2volume(symhkl_sub, astar, bstar, cstar, ithreshold=thr)
-
-print '### saving data ... '
-if __name__ == "__main__":
-	print ('start saving files... ')  
-	datawr = f.create_dataset('hkl', hkl.shape)
-	datawr[...] = hkl
-	datawr = f.create_dataset('distri', backg.shape)
-	datawr[...] = backg
-	datawr = f.create_dataset('volume', volumelist.shape)
-	datawr[...] = volumelist
-	datawr = f.create_dataset('symhkl', symhkl.shape)
-	datawr[...] = symhkl
-	datawr = f.create_dataset('anisohkl', symhkl_sub.shape)
-	datawr[...] = symhkl_sub
-	f.close() 
+print ('### start saving files... ')  
+zf.h5modify(args.i, name+'symData', symData)
+zf.h5modify(args.i, name+'anisoData', subData)
+zf.h5modify(args.i, name+'backg', backg)
