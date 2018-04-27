7435e75f5c6598832454e238ab93bb183ec8ce7e
zhenwork <zhensu@stanford.edu>
2018-04-27 01:26:40 -0700

    update

--- volumeProcess.py
+++ volumeProcess.py
@@ -8,6 +8,9 @@ parser.add_argument("-i","--i", help="save folder", default=".", type=str)
 parser.add_argument("-thrmin","--thrmin", help="min value", default=-100, type=int)
 parser.add_argument("-thrmax","--thrmax", help="max value", default=1000, type=int)
 parser.add_argument("-name","--name", help="name", default="", type=str)
+parser.add_argument("-sym","--sym", help="process", default=True, type=bool)
+parser.add_argument("-sub","--sub", help="process", default=True, type=bool)
+parser.add_argument("-hkl2xyz","--hkl2xyz", help="process", default=True, type=bool)
 args = parser.parse_args()
 thr = (args.thrmin, args.thrmax)
 zf = iFile()
@@ -22,40 +25,54 @@ print '### bstar = ', bstar
 print '### cstar = ', cstar
 print '### threshold: ', thr
 
-print '### reading volume ... '
-rawData = zf.h5reader(args.i, 'intens')
-print '### max/min: ', np.amin(rawData), np.amax(rawData)
 
-print '### symmetrizing data ...'
-symData = lauesym(rawData, ithreshold=thr)
-print '### max/min: ', np.amin(symData), np.amax(symData)
 
-print '### background calculation ... '
-[backg, radius] = distri_alg2(symData, astar, bstar, cstar, ithreshold=thr, iscale=4, iwindow=5)
-print '### bgd max/min: ', np.amin(backg), np.amax(backg)
+if args.sym or args.sub or args.hkl2xyz:
+	print '### reading volume ... '
+	rawData = zf.h5reader(args.i, 'intens')
+	print '### max/min: ', np.amin(rawData), np.amax(rawData)
 
-#backg = meanf(backg, _scale=5, clim=(0.1, 50))
 
-print '### subtracting background ... '
-subData = symData.copy()
-[nnxx, nnyy, nnzz] = symData.shape
-for i in range(nnxx):
-	for j in range(nnyy):
-		for k in range(nnzz):
-			intr = radius[i,j,k]
-			subData[i,j,k] -= backg[intr]
-print '### max/min: ', np.amin(subData), np.amax(subData)
+if args.sym:
+	print '### symmetrizing data ...'
+	symData = lauesym(rawData, ithreshold=thr)
+	print '### max/min: ', np.amin(symData), np.amax(symData)
+	zf.h5modify(args.i, args.name+'symData', symData)
+else:
+	symData = rawData.copy()
 
-if np.amax( np.abs( Smat-np.eye(3) ) )>1e-3:
-	print "### converting hkl to xyz coordinate"
-	anisoData = hkl2volume(subData, astar, bstar, cstar, ithreshold=thr)
-	print '### max/min: ', np.amin(anisoData), np.amax(anisoData)
-else: 
-	print "### This is already the best coordinate ... "
-	anisoData = subData.copy()
 
-print ('### start saving files... ')  
-zf.h5modify(args.i, args.name+'symData', symData)
-zf.h5modify(args.i, args.name+'subData', subData)
-zf.h5modify(args.i, args.name+'anisoData', anisoData)
-zf.h5modify(args.i, args.name+'backg', backg)
+if args.sub:
+	print '### background calculation ... '
+	[backg, radius] = distri_alg2(symData, astar, bstar, cstar, ithreshold=thr, iscale=4, iwindow=5)
+	print '### bgd max/min: ', np.amin(backg), np.amax(backg)
+
+	#backg = meanf(backg, _scale=5, clim=(0.1, 50))
+
+	print '### subtracting background ... '
+	subData = symData.copy()
+	[nnxx, nnyy, nnzz] = symData.shape
+	for i in range(nnxx):
+		for j in range(nnyy):
+			for k in range(nnzz):
+				intr = radius[i,j,k]
+				subData[i,j,k] -= backg[intr]
+	print '### max/min: ', np.amin(subData), np.amax(subData)
+	zf.h5modify(args.i, args.name+'subData', subData)
+	zf.h5modify(args.i, args.name+'backg', backg)
+else:
+	subData = symData.copy()
+
+
+if args.hkl2xyz:
+	if np.amax( np.abs( Smat-np.eye(3) ) )>1e-3:
+		print "### converting hkl to xyz coordinate"
+		anisoData = hkl2volume(subData, astar, bstar, cstar, ithreshold=thr)
+		print '### max/min: ', np.amin(anisoData), np.amax(anisoData)
+	else: 
+		print "### This is already the best coordinate ... "
+		anisoData = subData.copy()
+
+	print ('### start saving files... ')  
+
+	zf.h5modify(args.i, args.name+'anisoData', anisoData)
