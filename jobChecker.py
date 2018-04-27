505d0444cd2815167190270778e66837cd2970a8
zhenwork <zhensu@stanford.edu>
2018-04-27 00:23:41 -0700

    update

--- jobChecker.py
+++ jobChecker.py
@@ -6,4 +6,5 @@ parser.add_argument("-i","--i", help="save folder", default=".", type=str)
 args = parser.parse_args()
 
 if args.i == '.' or not os.path.exists(args.i): 
+	print "### no such file"
 	raise Exception('no such file')
