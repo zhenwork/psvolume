60d91ad97382ac6daeb5d73e90b6bae21788bacd
zhenwork <zhensu@stanford.edu>
2018-04-27 00:25:15 -0700

    update

--- jobChecker.py
+++ jobChecker.py
@@ -5,6 +5,8 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-i","--i", help="save folder", default=".", type=str)
 args = parser.parse_args()
 
+tic = time.time()
 if args.i == '.' or not os.path.exists(args.i): 
-	print "### no such file"
+	toc = time.time()
+	print "###"+str(toc-tic).rjust(5)+"   "+ "### no such file"
 	raise Exception('no such file')
