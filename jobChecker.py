34dc5347a4105abb2a51ac4bf993f6d62438d693
zhenwork <zhensu@stanford.edu>
2018-04-27 00:29:47 -0700

    update

--- jobChecker.py
+++ jobChecker.py
@@ -6,6 +6,7 @@ parser.add_argument("-i","--i", help="save folder", default=".", type=str)
 args = parser.parse_args()
 
 tic = time.time()
+print args.i
 if args.i == '.' or not os.path.exists(args.i): 
 	toc = time.time()
 	print "###"+str(toc-tic).rjust(5)+"   "+ "### no such file"
