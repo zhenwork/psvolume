04e7b1136e7520fa635cea63b8b5e12c465fcb12
zhenwork <zhensu@stanford.edu>
2018-04-30 18:51:34 -0700

    update

--- imageProcessMaster.py
+++ imageProcessMaster.py
@@ -13,20 +13,23 @@ import argparse
 comm_rank = MPI.COMM_WORLD.Get_rank()
 comm_size = MPI.COMM_WORLD.Get_size()
 parser = argparse.ArgumentParser()
+parser.add_argument("-i","--i", help="read folder", default=".", type=str)
 parser.add_argument("-o","--o", help="save folder", default=".", type=str)
 parser.add_argument("-num","--num", help="num of images to process", default=-1, type=int)
 args = parser.parse_args()
 
 zf = iFile()
-if args.num==-1:
-	[num, allFile] = zf.counterFile(args.o+'/rawImage', title='.slice')
-else: num = int(args.num)
+if not (args.i).endswith('/'): args.i = args.i+'/'
+[num, allFile] = zf.counterFile(args.i, title='.slice')
+path = args.i[0:(len(args.i)-args.i[::-1].find('/',1))];
+prefix = allFile[0][0:(len(allFile[0])-allFile[0][::-1].find('_',1))];
+if args.num !=-1: num = int(args.num)
 sep = np.linspace(0, num, comm_size+1).astype('int')
 
 
 ## get image information from sample pattern
-Geo = zf.get_image_info(args.o+'/rawImage/rawImage_00000.slice')
-image = zf.h5reader(args.o+'/rawImage/rawImage_00000.slice', 'image')
+Geo = zf.get_image_info(args.i+'/00000.slice')
+image = zf.h5reader(args.i+'/00000.slice', 'image')
 
 ascale = solid_angle_correction(image, Geo)
 pscale = polarization_correction(image, Geo)
@@ -38,29 +41,29 @@ pscale  = 1./pscale
 
 if comm_rank == 0:
 	print '## Total number: '+str(num).rjust(5)
-	if not os.path.exists(args.o + '/mergeImage'): 
-		os.mkdir(args.o + '/mergeImage')
-	zf.h5modify(args.o+'/image.process', 'ascale' , ascale)
-	zf.h5modify(args.o+'/image.process', 'pscale' , pscale)
-	zf.h5modify(args.o+'/image.process', 'apscale', apscale)
+	if not os.path.exists(args.o): 
+		os.mkdir(args.o)
+	zf.h5modify(path+'/image.process', 'ascale' , ascale)
+	zf.h5modify(path+'/image.process', 'pscale' , pscale)
+	zf.h5modify(path+'/image.process', 'apscale', apscale)
 else:
-	while not os.path.exists(args.o + '/mergeImage'): pass
+	while not os.path.exists(args.o): pass
 
-mask = zf.h5reader(args.o+'/image.process', 'mask')
+mask = zf.h5reader(path+'/image.process', 'mask')
 for idx in range(sep[comm_rank], sep[comm_rank+1]):
-	fname = args.o+'/rawImage/rawImage_'+str(idx).zfill(5)+'.slice'
+	fname = args.i+'/'+str(idx).zfill(5)+'.slice'
 	Geo = zf.get_image_info(fname)
 	assert Geo['readout']=='image'
 	image = zf.h5reader(fname, 'image')
 
 	image *= apscale
-	image *= Geo['scale']
-	Geo['scale'] = 1.
+	#image *= Geo['scale']
+	#Geo['scale'] = 1.
 	
 	sumIntens = round(np.sum(image), 8)
 	image = remove_peak_alg1(image, mask=mask, sigma=15, cwin=(11,11))
 
-	fsave = args.o + '/mergeImage/mergeImage_'+str(idx).zfill(5)+'.slice'
+	fsave = args.o + '/'+str(idx).zfill(5)+'.slice'
 
 	zf.h5writer(fsave, 'readout', 'image') # image, event
 	zf.h5modify(fsave, 'image', image)
