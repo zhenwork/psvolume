fdb71899721262376ce605fcedfbfdba8f905eed
zhenwork <zhensu@stanford.edu>
2018-07-16 16:17:01 -0700

    update

--- imageMergeClient.py
+++ imageMergeClient.py
@@ -196,7 +196,7 @@ def ImageMerge_HKL_VOXEL(model3d, weight, image, Geo, Volume, Kpeak=False, voxel
 
 	for t in range(len(HKL)):
 
-		if (Image[t] < -0.01): continue
+		if (Image[t] < 0): continue
 		
 		hkl = HKL[t] + Vcenter
 		
