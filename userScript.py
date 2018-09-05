f20c6a47aa06d3203a61db952cf83bf4d2bcbb3c
zhenwork <36317218+zhenwork@users.noreply.github.com>
2018-09-05 11:55:38 -0700

    Update userScript.py

--- userScript.py
+++ userScript.py
@@ -64,10 +64,9 @@ def user_get_mask(Geo, fname=None):
 
 	if fname.endswith('.img'):
 		radius = make_radius(mask.shape, center=Geo['center'])
-		index = np.where(radius<10)
-		mask[index] = 0
 		index = np.where(data == 0 )
 		mask[index] = 0
+		mask[506:557, 471:517] = 0
 	else:
 		index = np.where(data > 100000)
 		mask[index] = 0
