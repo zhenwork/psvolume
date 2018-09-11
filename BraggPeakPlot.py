96d91d046854da20f1fced2a93fd382c515f0953
zhenwork <36317218+zhenwork@users.noreply.github.com>
2018-09-10 18:31:19 -0700

    Update BraggPeakPlot.py

--- BraggPeakPlot.py
+++ BraggPeakPlot.py
@@ -110,6 +110,120 @@ def CalBraggPeakIntensity_alg1(image, Geo, peakRing=0.15, backRing=0.40):
 
 
 
+@jit
+def CalBraggPeakIntensity_alg1(image, Geo, peakRing=0.15, backRing=0.40):
+	Vsize = 121
+	Vcenter = 60
+	Vsample = 1
+
+	peakIntensity = np.zeros((Vsize,Vsize,Vsize))
+	peakCounts = np.zeros((Vsize,Vsize,Vsize))
+	backIntensity = np.zeros((Vsize,Vsize,Vsize))
+	backCounts = np.zeros((Vsize,Vsize,Vsize))
+
+	voxel = Geometry(image, Geo)
+	Image = image.ravel()
+	Rot = Geo['rotation']
+	HKL = Vsample*(Rot.dot(voxel)).T
+
+	pmark = np.zeros(Image.shape)
+
+	for t in range(len(HKL)):
+
+		if (Image[t] < 0): continue
+		
+		hkl = HKL[t] + Vcenter
+		
+		h = hkl[0] 
+		k = hkl[1] 
+		l = hkl[2] 
+		
+		inth = int(round(h)) 
+		intk = int(round(k)) 
+		intl = int(round(l)) 
+
+		if (inth<0) or inth>(Vsize-1) or (intk<0) or intk>(Vsize-1) or (intl<0) or intl>(Vsize-1): 
+			continue
+		
+		hshift = abs(h/Vsample-round(h/Vsample))
+		kshift = abs(k/Vsample-round(k/Vsample))
+		lshift = abs(l/Vsample-round(l/Vsample))
+
+		if (hshift<=peakRing) and (kshift<=peakRing) and (lshift<=peakRing):
+			peakIntensity[inth,intk,intl] += Image[t] 
+			peakCounts[inth,intk,intl] += 1.
+			pmark[t] += 10.
+		elif max(hshift, kshift, lshift)<backRing:
+			backIntensity[inth,intk,intl] += Image[t] 
+			backCounts[inth,intk,intl] += 1.
+			pmark[t] += 5.
+
+	## calculate average background (per pixel)
+	index = np.where(backCounts>0.5)
+	backIntensity[index] /= backCounts[index]
+	
+	## peakIntensity subtract the average background*peak numbers
+	subIntensity = peakIntensity - backIntensity*peakCounts
+	aveSubIntensity = subIntensity.copy()
+	
+	## calculate average subIntensity (only for calculating index)
+	index = np.where(peakCounts>0.5)
+	aveSubIntensity[index] = subIntensity[index]/peakCounts[index]
+
+	## index is all HKLs that fit the condition
+	keepVolume = np.zeros(peakCounts.shape).astype(int)
+	index = np.where((peakCounts>=5)*(backCounts>=10)*(aveSubIntensity>5.)==True)
+	keepVolume[index] = 1
+	
+	## calculate sum of peak intensity at suitable HKLs
+	data = subIntensity[index].copy()
+	(A,B) = (np.sum(data), len(data))
+
+
+	pmark.shape = image.shape
+
+
+
+	## This is just a test of the algorithm
+	peakTest = Image.copy()
+	for t in range(len(HKL)):
+
+		if (Image[t] < 0): continue
+		
+		hkl = HKL[t] + Vcenter
+		
+		h = hkl[0] 
+		k = hkl[1] 
+		l = hkl[2] 
+		
+		inth = int(round(h)) 
+		intk = int(round(k)) 
+		intl = int(round(l)) 
+
+		if (inth<0) or inth>(Vsize-1) or (intk<0) or intk>(Vsize-1) or (intl<0) or intl>(Vsize-1): 
+			continue
+		
+		hshift = abs(h/Vsample-round(h/Vsample))
+		kshift = abs(k/Vsample-round(k/Vsample))
+		lshift = abs(l/Vsample-round(l/Vsample))
+
+		if keepVolume[inth, intk, intl] < 1:
+			continue
+		if max(hshift, kshift, lshift)<0.15:
+			continue
+		elif max(hshift, kshift, lshift)<0.30:
+			peakTest[t] = 1000
+
+	peakTest.shape = image.shape
+
+	##############################
+
+
+	return (A,B,pmark, peakTest)
+
+
+
+
 
 ### create the mask
 filename = folder_i + '/00000.slice'
@@ -149,11 +263,12 @@ for idx in range(sep[comm_rank], sep[comm_rank+1]):
 	image[index] = -1
 
 
-	[peakIntensity, peakCounts, pmark] = CalBraggPeakIntensity_alg1(image, Geo, peakRing=0.15, backRing=0.30 )
+	[peakIntensity, peakCounts, pmark, peakTest] = CalBraggPeakIntensity_alg1(image, Geo, peakRing=0.15, backRing=0.30 )
 	BraggPeakIntensity[idx] = peakIntensity
 	BraggPeakCounts[idx] = peakCounts
 
 	zf.h5writer("./pmark/%.5d.slice"%idx, "pmark", pmark)
+	zf.h5writer("./peakTest/%.5d.slice"%idx, "peakTest", peakTest)
 
 	print "### Rank %.4d finished %.4d-%.4d-%.4d"%(comm_rank, sep[comm_rank], idx, sep[comm_rank+1])
 
