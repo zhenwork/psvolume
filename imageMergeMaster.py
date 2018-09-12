c9e8e37e6cfdc9fd074f009960c74a6fce891739
zhenwork <36317218+zhenwork@users.noreply.github.com>
2018-09-12 16:09:24 -0700

    Update imageMergeMaster.py

--- imageMergeMaster.py
+++ imageMergeMaster.py
@@ -100,8 +100,8 @@ if comm_rank == 0:
 	ThisFile = zf.readtxt(os.path.realpath(__file__))
 	zf.h5writer(pathIntens, 'execute', ThisFile)
 	zf.h5modify(pathIntens, 'sampling', Vol['volumeSampling'])
-	zf.h5modify(pathIntens, 'intens', model3d, chunks=(1, Vol['volumeSize'], Vol['volumeSize']), opts=7)
-	zf.h5modify(pathIntens, 'weight', weight,  chunks=(1, Vol['volumeSize'], Vol['volumeSize']), opts=7)
+	zf.h5modify(pathIntens, 'intens', model3d, chunks=(1, model3d.shape[1],model3d.shape[2]), opts=7)
+	zf.h5modify(pathIntens, 'weight', weight,  chunks=(1,  weight.shape[1], weight.shape[2]), opts=7)
 	zf.h5modify(pathIntens, 'Smat', Smat)
 
 else:
