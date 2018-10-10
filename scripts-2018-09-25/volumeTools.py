5b1e36ce34239733a969b0df2ea6a8b29de48f9f
zhenwork <zhenwork@users.noreply.github.com>
2018-10-10 14:30:36 -0700

    update

--- scripts-2018-09-25/volumeTools.py
+++ scripts-2018-09-25/volumeTools.py
@@ -62,8 +62,12 @@ def lauesym(idata, ithreshold=(-100,1000)):
 				mk = num_samp-1-k
 				
 				# FIXME: only applicable to P21 symmetry
-				pairs = np.array([ idata[i,j,k], idata[mi,mj,mk], idata[mi,j,mk], idata[i,mj,k] ]) #P1211
+				# pairs = np.array([ idata[i,j,k], idata[mi,mj,mk], idata[mi,j,mk], idata[i,mj,k] ]) #P1211
 				
+				# FIXME: only applicable to friedel symmetry
+				pairs = np.array([ idata[i,j,k], idata[mi,mj,mk] ]) #P1211
+				
+
 				# FIXME: only applicable for snc data
 				#pairs = np.array([ idata[i,j,k], idata[mi,mj,k], idata[mj,i,k], idata[j,mi,k], idata[mi,mj,mk], idata[i,j,mk], idata[j,mi,mk], idata[mj,i,mk]])
 				
