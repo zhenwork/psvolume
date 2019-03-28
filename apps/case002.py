5dd180f0f20619209c23ecf7d9e393466cd62fc1
zhenwork <zhenwork@users.noreply.github.com>
2019-03-28 05:15:48 -0700

    Create case002.py

---
+++ apps/case002.py
@@ -0,0 +1,64 @@
+"""
+1. Merge without background subtraction 
+2. Scale based on radial prodile
+"""
+
+import os,sys
+import numpy as np
+sys.path.append('/reg/neh/home5/zhensu/Develop/psvolume')
+import scripts.expAgent as expAgent
+from scripts.mpidata import *
+import scripts.fileManager as fileManager
+from numba import jit
+
+PsvolumeManager = fileManager.PsvolumeManager()
+FileSystem = fileManager.FileSystem()
+
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("-fsave","--fsave", help="save folder", default=None, type=str)
+parser.add_argument("-fname","--fname", help="input files", default=None, type=str)
+parser.add_argument("-xds","--xds", help="xds file", default=".", type=str)
+args = parser.parse_args()
+
+files = FileSystem.listFileWithFind(args.fname.replace("#####", "*"))
+nmax = len(files)
+
+assign = np.linspace(1, nmax+1, comm_size+1).astype(int)
+print ">>>> %3d process [ %4d, %4d ) in %4d"%(comm_rank, assign[comm_rank], assign[comm_rank+1], nmax)
+
+
+
+
+## reference pattern ############################
+refile = args.fname.replace("#####", "00001")
+
+
+imageAgent = expAgent.ImageAgent()
+imageAgent.loadImage(refile)
+imageAgent.loadImage(args.xds)
+imageAgent.preprocess()
+imageAgent.radprofile()
+refData = imageAgent.todict()
+imageAgent = None
+## reference pattern ############################
+
+
+for idx in range(assign[comm_rank], assign[comm_rank+1]):
+    
+    filename = args.fname.replace("#####", "%.5d"%idx)
+    fsave = args.fsave.replace("#####", "%.5d"%idx)
+    print "Loading: ", filename
+    
+
+    imageAgent = expAgent.ImageAgent()
+    imageAgent.loadImage(filename)
+    imageAgent.loadImage(args.xds) 
+    imageAgent.preprocess()
+    imageAgent.radprofile()
+    imageAgent.scaling(reference = refData, mode="rad", rmin=160, rmax=400)
+    
+
+    PsvolumeManager.psvm2h5py(imageAgent.todict(), fsave)
+    imageAgent = None
