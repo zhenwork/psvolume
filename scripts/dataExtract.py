7ed3c9c1eec4f2b19328e5d73273193d0294858e
zhenwork <zhenwork@users.noreply.github.com>
2019-07-25 00:13:00 -0700

    update

--- scripts/dataExtract.py
+++ scripts/dataExtract.py
@@ -116,6 +116,7 @@ def specialparams(notation="wtich"):
         mask[1255:1305+2,1735:2000] = 0
         mask[1255:1310+2,2000:2463] = 0
         psvmParms["mask"] = mask.T
+        psvmParms["firMask"] = mask.T
         return psvmParms
     elif notation == "old":
         psvmParms = {}
@@ -124,6 +125,7 @@ def specialparams(notation="wtich"):
         mask[1255:1305,1735:2000] = 0
         mask[1255:1310,2000:2463] = 0
         psvmParms["mask"] = mask.T
+        psvmParms["firMask"] = mask.T
         return psvmParms
     else:
         print "!! No Special Params"
