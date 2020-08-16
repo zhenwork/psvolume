7b293d1f6b3d7c80d29394a9b3d6fc0b02a25222
zhenwork <zhenwork@users.noreply.github.com>
2020-08-15 21:32:40 -0700

    v3.0.1

--- scripts/dataExtract.py
+++ scripts/dataExtract.py
@@ -1,12 +1,54 @@
 """
 Angstrom (A): wavelength, crystal lattice
 """
-
+import json
 import numpy as np  
 import crystalTools
 import fileManager
 import imageTools
 PsvolumeManager = fileManager.PsvolumeManager()
+
+
+# "../DIALS/G150T-2/dials.report.html"
+def dials_report(fname):
+    import codecs
+    f=codecs.open(fname, 'r')
+    content = f.readlines()
+
+    counter = 0
+    for line in content:
+        if "smoothly-varying scaling term" in line:
+            counter += 1
+            if counter == 2:
+                break
+    content = None
+    startx = line.index('"x": [')
+    stopx = line.index('], "xaxis"')
+
+    xvalue = []
+    for data in line[startx+6:stopx].split(","):
+        xvalue.append(float(data))
+
+    starty = line.index('"y": [')
+    stopy = line.index('], "yaxis"')
+
+    yvalue = []
+    for data in line[starty+6:stopy].split(","):
+        yvalue.append(float(data))    
+    
+    return xvalue,yvalue
+
+
+def getscale(x,y,theta):
+    if x[0] > theta:
+        return y[0]
+    if x[-1] < theta:
+        return y[-1]
+    a = np.where(np.array(x)-theta>0)[0][0] - 1
+    b = a+1 
+    return (y[b] - y[a]) / (x[b] - x[a]) * (theta - x[a]) + y[a]
+
+
 
 def xds2psvm(fileGXPARMS):
     """
@@ -79,6 +121,50 @@ def cbf2psvm(fileName):
     cbfhandler = None
     return psvmParms
 
+
+def expt2psvm(fexpt):
+    """
+    The unit of lattice constant, invAmat is A
+    The unit of angles is degree
+    The unit of pixelSize is meter
+    The unit of detectorDistance is meter
+    The unit of waveLength is A
+    """
+    def length(arr):
+        return np.linalg.norm(arr)
+    def angle(x,y):
+        return np.arccos(x.dot(y)/length(x)/length(y)) * 180. / np.pi
+
+    psvmParms = {}
+
+    data = json.load(open(fexpt,"r"))
+
+    psvmParms['pixelSize']    = data["detector"][0]["panels"][0]["pixel_size"][0]
+    psvmParms['polarization'] = -1.0
+    psvmParms['waveLength']   = data["beam"][0]["wavelength"]
+    psvmParms['angleStep']    = data["scan"][0]["oscillation"][1]
+    psvmParms['detectorDistance'] = - float(data["detector"][0]["panels"][0]["origin"][2])
+    psvmParms['detectorCenter']   =  (-float(data["detector"][0]["panels"][0]["origin"][0])/psvmParms['pixelSize'], float(data["detector"][0]["panels"][0]["origin"][1])/psvmParms['pixelSize'])
+
+
+    ## calculate the Amat matrix
+    ## GXPARMS saves real space vecA, vecB, vecC, which is invAmat
+    invAmat = np.array([data["crystal"][0]["real_space_a"],data["crystal"][0]["real_space_b"],data["crystal"][0]["real_space_c"]])
+    Amat = np.linalg.inv(invAmat)
+
+    ## calculate B matrix
+    ## Bmat is the user's defined coordinates
+    (a,b,c,alpha,beta,gamma) = [length(invAmat[0]), length(invAmat[1]), length(invAmat[2]), angle(invAmat[1], invAmat[2]), angle(invAmat[0], invAmat[2]), angle(invAmat[0], invAmat[1])]
+    latticeConstant = np.array([a,b,c,alpha,beta,gamma])
+    Bmat = crystalTools.standardBmat(latticceConstant=(a,b,c,alpha,beta,gamma))
+    
+    psvmParms["latticeConstant"] = latticeConstant
+    psvmParms["Amat"] = Amat
+    psvmParms["Bmat"] = Bmat
+    
+    return psvmParms
+
+
 def h5py2psvm(fileName):
     psvmParams = PsvolumeManager.h5py2psvm(fileName)
     return psvmParams
@@ -100,6 +186,8 @@ def loadfile(filename, fileType=None):
         return h5py2psvm(filename)
     elif fileType.lower() in ["numpy", "npy"]:
         return numpy2psvm(filename)
+    elif fileType.lower() in ["expt"]:
+        return expt2psvm(filename)
     else:
         print "!! Only Support .xds, .cbf, .h5, .npy"
         return {}
